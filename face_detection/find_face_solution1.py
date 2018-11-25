7d213a368430bbbb4c73f2a1a3ff70e514071c8f
病名は愛 <yehaojie@umich.edu>
2018-11-24 17:15:21 -0800

    #

--- face_detection/find_face_solution1.py
+++ face_detection/find_face_solution1.py
@@ -2,7 +2,7 @@ import cv2
 
 def detect(path):
     img = cv2.imread(path)
-    cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
     rects = cascade.detectMultiScale(img, 1.3, 4, cv2.CASCADE_SCALE_IMAGE, (20,20))
 
     if len(rects) == 0:
